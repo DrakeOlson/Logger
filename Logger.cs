915339a13968ba6c4f54fdeca96576c2fd303b01
Drake <drake.olson@jacks.sdstate.edu>
2017-04-08 22:10:57 -0500

    Logger Commit

---
+++ Logger.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+namespace Logger
+{
+    class Logger
+    {
+        private FileStream logFile { get; set; }
+        public Logger(string fileName)
+        {
+            try
+            {
+                logFile = File.Create($"{fileName}.log");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+        public void WriteLine(string line)
+        {
+            try
+            {
+                string formattedLine = $"{DateTime.Now} -- {line}\n";
+                Byte[] writtenLine = new UTF8Encoding(true).GetBytes($"{formattedLine}\n");
+                logFile.Write(writtenLine, 0, writtenLine.Length);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+    }
+}
